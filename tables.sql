f1e8edce2920953cfd3958f767c093ac6f4b38a6
gioxatz <132553598+gioxatz@users.noreply.github.com>
2023-05-28 22:20:42 +0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -22,7 +22,7 @@ CREATE TABLE `lib1`.`books` (
 `copies` INT NOT NULL , 
 `image` TEXT NOT NULL , 
 `summary` TEXT NOT NULL , 
-`available_copies` INT NOT NULL,
+`available_copies` INT NOT NULL DEFAULT `copies`,
 PRIMARY KEY (`ISBN`, `schoolID`)) ENGINE = InnoDB;
 
 ALTER TABLE `books` ADD FOREIGN KEY (`schoolID`) REFERENCES `schools`(`schoolID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
@@ -123,7 +123,7 @@ CREATE TABLE `lib1`.`loans` (
 `loanID` INT NOT NULL AUTO_INCREMENT , 
 `ISBN` BIGINT NOT NULL , 
 `loan_date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-`end_date` DATE NOT NULL , 
+`end_date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
 PRIMARY KEY (`loanID`)) ENGINE = InnoDB;
 
 ALTER TABLE `loans` ADD `active` BOOLEAN NOT NULL DEFAULT TRUE AFTER `end_date`;
@@ -134,7 +134,7 @@ CREATE TABLE `lib1`.`reservations` (
 `resID` INT NOT NULL AUTO_INCREMENT , 
 `ISBN` BIGINT NOT NULL , 
 `res_date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-`end_date` DATE NOT NULL , 
+`end_date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
 PRIMARY KEY (`resID`)) ENGINE = InnoDB;
 
 ALTER TABLE `reservations` ADD FOREIGN KEY (`ISBN`) REFERENCES `books`(`ISBN`) ON DELETE RESTRICT ON UPDATE RESTRICT;
@@ -204,6 +204,8 @@ CREATE INDEX unames ON users (username);
 CREATE INDEX pasrs ON users (password);
 
 
+SET GLOBAL event_scheduler = ON;
+
 
 
 
@@ -225,7 +227,7 @@ CREATE TRIGGER `increase_num_reserv_trigger` AFTER INSERT ON `has_reserv`
 END
 
 
-SET GLOBAL event_scheduler = ON;
+
 
 DELIMITER //
 
@@ -237,13 +239,11 @@ BEGIN
     DECLARE user_type VARCHAR(10);
     DECLARE user_id INT;
     
-    -- Delete expired reservations from has_reserv table
     DELETE hr
     FROM has_reserv hr
     JOIN reservations r ON hr.resID = r.resID
     WHERE DATEDIFF(CURRENT_DATE, r.res_date) > 7;
     
-    -- Retrieve the user ID and user type for affected reservations
     SELECT r.userID, CASE
         WHEN EXISTS(SELECT * FROM student WHERE userID = r.userID) THEN 'student'
         WHEN EXISTS(SELECT * FROM teacher WHERE userID = r.userID) THEN 'teacher'
@@ -251,10 +251,8 @@ BEGIN
     FROM reservations r
     WHERE DATEDIFF(CURRENT_DATE, r.res_date) > 7;
     
-    -- Delete expired reservations from reservations table
     DELETE FROM reservations WHERE DATEDIFF(CURRENT_DATE, res_date) > 7;
     
-    -- Increment num_reserv by 1 for the respective user
     IF user_type = 'student' THEN
         UPDATE student SET num_reserv = num_reserv + 1 WHERE userID = user_id;
     ELSEIF user_type = 'teacher' THEN
